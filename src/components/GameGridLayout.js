1b8ede3b0e76aeea6e475cbdd794f848e1d38490
oh <w.juthatip@gmail.com>
2017-06-25 12:33:03 +0700

    create game template

---
+++ src/components/GameGridLayout.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default () => {
+  return(
+    <div>
+      <div className="player"><span>1</span></div>
+      <div className="player"><span>2</span></div>
+      <div className="player"><span>3</span></div>
+      <div className="player"><span>4</span></div>
+      <div className="player"><span>5</span></div>
+      {/*<!-- 6 -->*/}
+      <div className="player"><span>6</span></div>
+      <div className="player"><span>7</span></div>
+      <div className="player"><span>8</span></div>
+      <div className="player"><span>9</span></div>
+      <div className="player"><span>10</span></div>
+      {/*<!-- 11 -->*/}
+      <div className="player"><span>11</span></div>
+      <div className="player"><span>12</span></div>
+      <div className="player"><span>13</span></div>
+      <div className="player"><span>14</span></div>
+      <div className="player"><span>15</span></div>
+      {/*<!-- 16 -->*/}
+      <div className="player"><span>16</span></div>
+      <div className="player"><span>17</span></div>
+      <div className="player"><span>18</span></div>
+      <div className="player"><span>19</span></div>
+      <div className="player"><span>20</span></div>
+      {/*<!--  21-->*/}
+      <div className="player"><span>21</span></div>
+      <div className="player"><span>22</span></div>
+      <div className="player"><span>23</span></div>
+      <div className="player"><span>24</span></div>
+      <div className="player"><span>25</span></div>
+    </div>
+  )
+}
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -3,6 +3,15 @@ import { connect } from 'react-redux'
 // import { browserHistory } from 'react-router'
 
 import { room } from '../actions'
+import GameGridLayout from '../components/GameGridLayout'
+
+import imgPlayer from '../images/player.png'
+import imgEnemy from '../images/enemy.png'
+import effBomb1 from '../images/effect-bomb/b1.png'
+import effBomb2 from '../images/effect-bomb/b2.png'
+import effBomb3 from '../images/effect-bomb/b3.png'
+import eff4horizon from '../images/effect-bomb/b4-horizon.png'
+import eff4vertical from '../images/effect-bomb/b4-vertical.png'
 
 class Game extends Component {
 
@@ -84,20 +93,31 @@ class Game extends Component {
   }
 
   renderOwnerTurn() {
-    let shotBtn = <button onClick={ () => this.fire('owner') }>Fire</button>
+    let shotBtn = <button className="btn-start btn-pre btn-ok btn-shoot" onClick={ () => this.fire('owner') }>Fire</button>
     if (this.userType === 'joiner') shotBtn = ''
     return (
       <div className={this.state.ownerClass}>
-        1 <input type="text" onChange={this.handleOwnerFire} value={this.state.ownerFire} /> {shotBtn}
+        <div className="player-info">
+          <img src={imgPlayer} className="" alt={imgPlayer} />
+          &nbsp;Timer: <span className="red">10</span> sec
+        </div>
+        <GameGridLayout />
+        1 <input type="text" onChange={this.handleOwnerFire} value={this.state.ownerFire} className="gameinput" />
+        {shotBtn}
       </div>
     )
   }
 
   renderJoinerTurn() {
-    let shotBtn = <button onClick={ () => this.fire('joiner') }>Fire</button>
+    let shotBtn = <button className="btn-start btn-pre btn-ok btn-shoot btn-enemy" onClick={ () => this.fire('joiner') }>Fire</button>
     if (this.userType === 'owner') shotBtn = ''
     return (
       <div className={this.state.joinerClass}>
+        <div className="player-info enemy-turn">
+          <img src={imgEnemy} className="" alt={imgEnemy} />
+          &nbsp;Enemy Turn!
+        </div>
+        <GameGridLayout />
         2 <input type="text" onChange={this.handleJoinerFire} value={this.state.joinerFire}/> {shotBtn}
       </div>
     )
@@ -105,12 +125,28 @@ class Game extends Component {
 
   render() {
     return (
-      <div>
-        <br />
-        <br /><br /><br /><br /><br /><br /><br /><br />
-        Game
+      <div className="water-wrap game">
+
+
         {this.renderOwnerTurn()}
         {this.renderJoinerTurn()}
+
+        {/*<div className="ship-wrap add-bomb">*/}
+          {/*<div className="b b1"><img src={effBomb1} alt={effBomb1} /></div>*/}
+          {/*<div className="b b2"><img src={effBomb2} alt={effBomb2} /></div>*/}
+          {/*<div className="b b3"><img src={effBomb3} alt={effBomb3} /></div>*/}
+          {/*<div className="b b4"><img src={eff4horizon} alt={eff4horizon} /></div>*/}
+        {/*</div>*/}
+
+        {/*<div className="ship-wrap">*/}
+          {/*<div className="b b1"><img src={effBomb1} alt={effBomb1} /></div>*/}
+          {/*<div className="b b2"><img src={effBomb2} alt={effBomb2} /></div>*/}
+          {/*<div className="b b3"><img src={effBomb3} alt={effBomb3} /></div>*/}
+          {/*<div className="b b4"><img src={eff4vertical} alt={eff4vertical} /></div>*/}
+        {/*</div>*/}
+
+        {/*<div className="btn-start btn-pre btn-ok btn-shoot">Shoot</div>*/}
+
       </div>
     )
   }
